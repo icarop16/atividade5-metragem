8c3c9d8dacf643075f69669a171ed1ffe758742f
icarop16 <pedroicaro161107@gmail.com>
2023-10-11 11:19:13 -0300

    Atividade Metragem

---
+++ pedroicaro.py
@@ -0,0 +1,6 @@
+#Digite uma metragem e veja isso em cm e mm
+Metro = float(input("digite o valor de metro que você vai converter:"))
+cm = Metro*100
+mm = Metro*1000
+print("O valor em ", Metro," em cm é:", cm)
+print("O valor em", Metro, "em mm é:", mm)
